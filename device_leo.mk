0945deb6cff0e544274a6277acbcf55ef92c5dab
Sportsstar89 <sjpirri@gmail.com>
2013-02-25 20:32:02 -0500

    Fix derps

--- device_leo.mk
+++ device_leo.mk
@@ -101,7 +101,7 @@ endif
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 # High-Density Res
 PRODUCT_LOCALES += hdpi mdpi
